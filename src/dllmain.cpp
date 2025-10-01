695d66a5c8ab31b2001a50888511743f1c028744
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-10-01 16:43:12 +0100

    Update to latest

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -2,7 +2,7 @@
 #include "items/ModItems.hpp"
 
 // Ran when the mod is loaded into the game by AmethystRuntime
-ModFunction void Initialize(AmethystContext& ctx, const Mod& mod) 
+ModFunction void Initialize(AmethystContext& ctx, const Amethyst::Mod& mod) 
 {
     // Initialize Amethyst mod backend
     Amethyst::InitializeAmethystMod(ctx, mod);
