56b6a213c82e23fb8439bc59feba78fffdf41aa0
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-10-10 13:45:30 +0100

    Custom Blocks Tutorial

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,5 +1,6 @@
 ﻿#include "dllmain.hpp"
 #include "items/ModItems.hpp"
+#include "blocks/ModBlocks.hpp"
 
 // Ran when the mod is loaded into the game by AmethystRuntime
 ModFunction void Initialize(AmethystContext& ctx, const Amethyst::Mod& mod) 
@@ -9,4 +10,7 @@ ModFunction void Initialize(AmethystContext& ctx, const Amethyst::Mod& mod)
 
     Amethyst::EventBus& bus = Amethyst::GetEventBus();
     bus.AddListener<RegisterItemsEvent>(&ModItems::RegisterItems);
+    bus.AddListener<RegisterBlocksEvent>(&ModBlocks::RegisterModBlocks);
+    bus.AddListener<RegisterItemsEvent>(&ModBlocks::RegisterBlockItems);
+    bus.AddListener<InitBlockGraphicsEvent>(&ModBlocks::InitBlockGraphics);
 }
